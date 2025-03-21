92b7a3de960fb3fa4b1e7d890ffc31ee3cf0357f
LouisCrl <louis.carlu@viacesi.fr>
2025-03-21 14:40:22 +0100

    acceuil.php dans template

---
+++ templates/acceuil.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TheGoodPlan - Trouvez votre stage</title>
+    <link rel="stylesheet" href="assets/styles.css">
+</head>
+
+<body>
+<?php include "header.html" ?>
+    <main class="accueil">
+        <h1>Trouvez votre stage facilement !</h1>
+        <div class="barre-recherche">
+            <input type="text" placeholder="Rechercher">
+        </div>
+        <section class="en-vedette">
+            <h2>Offres en vedette</h2>
+        <section class="offres">
+            <a href="details-offre.php">
+            <div class="offre rouge">
+                <div class="offre-head-apercu">
+                    <h4>Développeur FullStack H/F</h2>
+                    <div class="tags">
+                    <span class="localisation fond-rouge"><img src="assets/locIcon.png" width="24px">Paris</span>
+                    <span class="duree fond-rouge"><img src="assets/timeIcon.png" width="24px">6 mois</span>
+                    <span class="exp fond-rouge"><img src="assets/educationIcon.png" width="24px">Bac +2</span>
+                </div>
+                    <h5 class="inter-bold italic">Netflix</h3>
+                    <img class="logo-rouge" src="assets/netflixIcon.png" width="48">
+                </div>
+            </div>
+</a>
+<a href="details-offre.php">
+            <div class="offre jaune">
+                <div class="offre-head-apercu">
+                    <h4>Développeur FullStack H/F</h2>
+                    <div class="tags">
+                    <span class="localisation fond-jaune"><img src="assets/locIcon.png" width="24px">Paris</span>
+                    <span class="duree fond-jaune"><img src="assets/timeIcon.png" width="24px">6 mois</span>
+                    <span class="exp fond-jaune"><img src="assets/educationIcon.png" width="24px">Bac +2</span>
+                </div>
+                    <h5 class="inter-bold italic">McDonalds</h3>
+                    <img class="logo-jaune" src="assets/mcdonaldsIcon.png" width="48">
+                </div>
+            </div>
+</a>
+            <a href="details-offre.php">
+            <div class="offre vert">
+                <div class="offre-head-apercu">
+                    <h4>Développeur FullStack H/F</h2>
+                    <div class="tags">
+                    <span class="localisation fond-vert"><img src="assets/locIcon.png" width="24px">Paris</span>
+                    <span class="duree fond-vert"><img src="assets/timeIcon.png" width="24px">6 mois</span>
+                    <span class="exp fond-vert"><img src="assets/educationIcon.png" width="24px">Bac +2</span>
+                </div>
+                    <h5 class="inter-bold italic">Spotify</h3>
+                    <img class="logo-vert" src="assets/spotifyIcon.webp" width="48">
+                </div>
+            </div>
+</a>
+        </section>
+        </section>
+    </main>
+    <?php include "footer.html" ?>
+</body>
+
+</html>
